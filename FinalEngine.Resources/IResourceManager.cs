a2cdf07b039a68cbf304eb972ef31791b3c596a2
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-15 15:10:21 +1000

    Create IResourceManager interface

---
+++ FinalEngine.Resources/IResourceManager.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IResourceManager.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Resources
+{
+    public interface IResourceManager
+    {
+        T GetResource<T>(string filePath);
+
+        void RegisterLoader<T>(ResourceLoaderBase<T> loader)
+                    where T : IResource;
+    }
+}
