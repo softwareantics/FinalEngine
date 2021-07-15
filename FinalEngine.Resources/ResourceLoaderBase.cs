141b4534aff5a0532a8dae5336a9086c69338975
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-15 15:09:38 +1000

    Create ResourceLoaderBase

---
+++ FinalEngine.Resources/ResourceLoaderBase.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ResourceLoaderBase.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Resources
+{
+    public abstract class ResourceLoaderBase<T> : IResourceLoaderInternal
+        where T : IResource
+    {
+        public abstract T LoadResource(string filePath);
+
+        object IResourceLoaderInternal.LoadResource(string filePath)
+        {
+            return this.LoadResource(filePath);
+        }
+    }
+}
