e060948d1d59e16114702d17e427abadf64a2d76
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-15 15:04:03 +1000

    Create IResource interface

---
+++ FinalEngine.Resources/IResource.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="IResource.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Resources
+{
+    using System;
+
+    public interface IResource : IDisposable
+    {
+    }
+}
