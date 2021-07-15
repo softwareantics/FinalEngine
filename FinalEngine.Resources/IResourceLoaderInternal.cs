a5545747673f3d68249a701313bda5c598743eec
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-15 15:04:40 +1000

    Create IResourceLoaderInternal

---
+++ FinalEngine.Resources/IResourceLoaderInternal.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IResourceLoader.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Resources
+{
+    internal interface IResourceLoaderInternal
+    {
+        object LoadResource(string filePath);
+    }
+}
