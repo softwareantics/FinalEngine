b1ba41777b26a6280f43ea03db1abd2770314250
Mathew O'Dwyer <mathew.odwyer@protonmail.com>
2025-06-05 21:52:15 +1000

    Created IEngineModule.

---
+++ FinalEngine.Utilities/Modules/IEngineModule.cs
@@ -0,0 +1,10 @@
+// <copyright file="IEngineModule.cs" company="Software Antics">
+// Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Utilities.Modules;
+
+public interface IEngineModule
+{
+    void Load();
+}
