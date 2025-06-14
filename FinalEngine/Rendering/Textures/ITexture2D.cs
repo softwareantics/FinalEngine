c4f8dcf46218b3a87673c938bc4257e6eeb09c30
Mathew O'Dwyer <mathew.odwyer@protonmail.com>
2025-06-14 22:40:33 +1000

    Created ITexture2D

---
+++ FinalEngine/Rendering/Textures/ITexture2D.cs
@@ -0,0 +1,12 @@
+// <copyright file="ITexture2D.cs" company="Software Antics">
+//   Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering.Textures;
+
+public interface ITexture2D
+{
+    int Height { get; }
+
+    int Width { get; }
+}
